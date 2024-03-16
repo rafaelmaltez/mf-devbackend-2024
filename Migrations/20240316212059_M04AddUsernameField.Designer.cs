48b4569ab880c64981bf5687becadf6b8e3db444
Rafael Maltez <rafaelmaltez89@gmail.com>
2024-03-16 19:22:14 -0300

    refactor authorization

--- Migrations/20240316212059_M04AddUsernameField.Designer.cs
+++ Migrations/20240316212059_M04AddUsernameField.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using mf_devbackend_2024.Models;
 
@@ -11,9 +12,11 @@ using mf_devbackend_2024.Models;
 namespace mf_devbackend_2024.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240316212059_M04AddUsernameField")]
+    partial class M04AddUsernameField
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
