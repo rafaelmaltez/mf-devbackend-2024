63c5a5bb313615a19179e72da461fb56553b1fba
Rafael Maltez <rafaelmaltez89@gmail.com>
2024-03-16 11:34:42 -0300

    add Vehicle Model

---
+++ Models/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace mf_devbackend_2024.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Vehicle> Vehicles { get; set; }
+    }
+}
---
+++ Models/Vehicle.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mf_devbackend_2024.Models
+{
+    [Table("vehicles")]
+    public class Vehicle
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Name is mandatoy")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "PlateNumber is mandatoy")]
+        public int PlateNumber { get; set; }
+
+        [Required(ErrorMessage = "MakeYear is mandatoy")]
+        public int MakeYear { get; set; }
+
+        [Required(ErrorMessage = "ModelYear is mandatoy")]
+        public int ModelYear { get; set; }
+    }
+}
