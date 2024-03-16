2998fd678effd949ae8e4168abb6ccca54c43ba2
Rafael Maltez <rafaelmaltez89@gmail.com>
2024-03-16 13:14:35 -0300

    vehicle crud

---
+++ Controllers/VehiclesController.cs
@@ -0,0 +1,109 @@
+﻿using mf_devbackend_2024.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace mf_devbackend_2024.Controllers
+{
+    public class VehiclesController : Controller
+    {
+        private readonly AppDbContext _context;
+        public VehiclesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var vehicles = await _context.Vehicles.ToListAsync();
+            return View(vehicles);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Vehicle vehicle)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Vehicles.Add(vehicle);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(vehicle);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var vehicleData = await _context.Vehicles.FindAsync(id);
+
+            if (vehicleData == null)
+                return NotFound();
+
+            return View(vehicleData);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, Vehicle vehicle)
+        {
+            if (id != vehicle.Id)
+                return NotFound();
+            if (ModelState.IsValid)
+            {
+                _context.Vehicles.Update(vehicle);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(vehicle);
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var vehicleData = await _context.Vehicles.FindAsync(id);
+            
+            if(vehicleData == null)
+                return NotFound();
+            
+            return View(vehicleData);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var vehicleData = await _context.Vehicles.FindAsync(id);
+
+            if (vehicleData == null)
+                return NotFound();
+
+            return View(vehicleData);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var vehicleData = await _context.Vehicles.FindAsync(id);
+
+            if (vehicleData == null)
+                return NotFound();
+
+            _context.Vehicles.Remove(vehicleData);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index") ;
+        }
+    }
+}
--- Models/Vehicle.cs
+++ Models/Vehicle.cs
@@ -9,16 +9,20 @@ namespace mf_devbackend_2024.Models
         [Key]
         public int Id { get; set; }
 
-        [Required(ErrorMessage ="Name is mandatoy")]
+        [Required(ErrorMessage = "Name is mandatoy")]
+        [Display(Name = "Name")]
         public string Name { get; set; }
 
         [Required(ErrorMessage = "PlateNumber is mandatoy")]
+        [Display(Name = "Plate Number")]
         public int PlateNumber { get; set; }
 
         [Required(ErrorMessage = "MakeYear is mandatoy")]
+        [Display(Name = "Fabrication Year")]
         public int MakeYear { get; set; }
 
         [Required(ErrorMessage = "ModelYear is mandatoy")]
+        [Display(Name = "Model Year")]
         public int ModelYear { get; set; }
     }
 }
