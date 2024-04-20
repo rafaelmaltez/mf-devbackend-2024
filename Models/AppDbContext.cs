d746f4cf7511207fcf2fda482bab14a6a0b9b390
Rafael Maltez <rafaelmaltez89@gmail.com>
2024-04-20 13:21:32 -0300

    add admin seed

--- Models/AppDbContext.cs
+++ Models/AppDbContext.cs
@@ -11,5 +11,19 @@ namespace mf_devbackend_2024.Models
         public DbSet<Consumption> Consumptions { get; set; }
 
         public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasData(
+                new User
+                {
+                    Id = 1,
+                    Name = "Admin",
+                    UserName = "admin",
+                    Password = "$2a$10$ekA30WsbMQyQOJyJozk31eRathzdWM.PMuPS0FZ6ctTIH1Ajtl3Ny",
+                    Profile = 0
+                }
+            );
+        }
     }
 }
